a6d5962395ea3fb26a35e7bc68afc5c286ee0973
Eric Baack <baacer01@luther.edu>
2021-03-20 10:41:11 -0500

    Added state list: builds sites from state selection

--- js/controller.js
+++ js/controller.js
@@ -3,28 +3,68 @@
 /* jshint node: true */
 'use strict';
 
-
-const allCounties = {
-    "Anoka":["003","1002"],
-    "Becker":["005","2013"],
-    "Carlton":["017","7414"],
-    "Cook":["031","7810"],
-    "Dakota":["037","480"],
-    "Hennepin":["053","0962"],
-    "Lyon":["083","4210"],
-    "Olmsted":["109","5008"],
-    "Ramsey":["123","0868"],
-    "Saint Louis":["137","7550"],
-    "Scott":["139","0505"], 
-    "Stearns":["145","3052"],
+const allStates = {
+    "Illinois":17,
+    "Iowa":19,
+    "Minnesota":27,
+    "Nebraska":31,
+    "Wisconsin":55
 };
 
+// const allCounties = {
+//     "Anoka":["003","1002"],
+//     "Becker":["005","2013"],
+//     "Carlton":["017","7414"],
+//     "Cook":["031","7810"],
+//     "Dakota":["037","480"],
+//     "Hennepin":["053","0962"],
+//     "Lyon":["083","4210"],
+//     "Olmsted":["109","5008"],
+//     "Ramsey":["123","0868"],
+//     "Saint Louis":["137","7550"],
+//     "Scott":["139","0505"], 
+//     "Stearns":["145","3052"],
+// };
+
+async function getSites(){
+    //console.log("getting sites");
+    let sc = document.querySelector("#state_name").selectedOptions[0].value;
+    //let url = `https://aqs.epa.gov/data/api/annualData/bySite?email=baacer01@luther.edu&key=ecruhare55&param=88101&bdate=20190618&edate=20190618&state=${state}&county=${county}&site=${site}`;
+    let url = `https://aqs.epa.gov/data/api/monitors/byState?email=baacer01@luther.edu&key=ecruhare55&param=88101&bdate=20190608&edate=20190608&state=${sc}`
+    //console.log("GPS url", url);
+    let sitedata = await fetch(url)
+            .then(response => response.json())
+            .catch(error => console.log(error));
+    //return [data.Data[0].latitude, data.Data[0].longitude]
+    //console.log(sitedata.Data[0].latitude, sitedata.Data[0].longitude);
+    let allSites = {};
+    for (let j of sitedata["Data"]){
+        let cCode = j['county_code'];
+        let sCode = j['site_number'];
+        let countyName = j['county_name'];
+        let cityName = j['city_name'];
+        let name_str = countyName.concat("-", cityName, ".  Monitor: ", sCode);
+        let siteID = cCode.concat("-", sCode)
+        //console.log("id: ", siteID, "ns: ", name_str, cCode, sCode)
+        if (allSites.hasOwnProperty(siteID)) { }
+        else { 
+            allSites[siteID] = [name_str, cCode, sCode];
+        }
+    }
+    //console.log(allSites);
+    populateSites(allSites); 
+    //console.log(allSites);
+    //return [data.Data[0].latitude, data.Data[0].longitude];
+}
+
 async function getData() {
-    let state = "27";
-    let cd = document.querySelector("#county_name").selectedOptions[0].value;
+    let state = document.querySelector("#state_name").selectedOptions[0].value;
+    let cd = document.querySelector("#site_name").selectedOptions[0].value;
+    console.log(cd);
     let cdn = cd.split(",");
-    let county = cdn[0];
-    let site = cdn[1]
+    let county = cdn[1];
+    let site = cdn[2]
+    console.log(state, county, site)
     //let county = document.querySelector("#county");
     //let site = document.querySelector("#site");
     let GPS_list = getGPS(state, county, site);
@@ -89,9 +129,22 @@ function populateSelectOption(elementId, optionsArray) {
     }
 }
 
+function populateSites(allSites) {
+    
+    let menu = document.querySelector("#site_name");
+    for (let opt in allSites) {
+        let newOption = document.createElement("option");
+        newOption.setAttribute("value", allSites[opt]);
+        //console.log(allSites[opt])
+        newOption.innerHTML = allSites[opt];
+        menu.appendChild(newOption);
+    }
+}
+
 window.onload = function() {
-    populateSelectOption("#county_name", allCounties); 
-    let cd = document.querySelector("#county_name").selectedOptions[0].value;
-    let cdn = cd.split(",");
-    console.log(cdn[0], cdn[1]);
+    populateSelectOption("#state_name", allStates); 
+    //populateSelectOption("#county_name", allCounties); 
+    //let cd = document.querySelector("#county_name").selectedOptions[0].value;
+    //let cdn = cd.split(",");
+    //console.log(cdn[0], cdn[1]);
 }
