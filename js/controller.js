1be2d4d983ae9b16a0d20bb39a86dd1ba8ecd2c4
hiepvo01 <vohi0101@luther.edu>
2021-03-21 01:44:49 -0500

    add year function

--- js/controller.js
+++ js/controller.js
@@ -2,7 +2,19 @@
 /* jshint browser: true */
 /* jshint node: true */
 'use strict';
-
+const years = []
+for (let year=2015; year < new Date().getFullYear() - 1; year++){
+    years.push(year)
+}
+let menu = document.querySelector("#year_name");
+menu.innerHTML = "";
+for (let opt of years) {
+    let newOption = document.createElement("option");
+    newOption.setAttribute("value", opt);
+    //console.log(allSites[opt])
+    newOption.innerHTML = opt;
+    menu.appendChild(newOption);
+}
 const allStates = {
     "Illinois":17,
     "Iowa":19,
@@ -10,6 +22,13 @@ const allStates = {
     "Nebraska":31,
     "Wisconsin":55
 };
+
+// Change title
+function changeTitle(title){
+    var origin = document.querySelector("#title"); 
+    origin.innerHTML = `Air pollution & Census data ${title}`;
+  }
+  
 
 // Populate states
 function populateSelectOption(elementId, optionsArray) {
@@ -22,8 +41,7 @@ function populateSelectOption(elementId, optionsArray) {
     }
 }
 
-// Populate sites
-// Need to clear past sites
+// Populate sites and years
 function populateSites(allSites) {
     
     let menu = document.querySelector("#site_name");
@@ -54,7 +72,9 @@ function populateSites(allSites) {
 
 async function getSites(){
     let sc = document.querySelector("#state_name").selectedOptions[0].value;
-    let url = `https://aqs.epa.gov/data/api/monitors/byState?email=baacer01@luther.edu&key=ecruhare55&param=88101&bdate=20190608&edate=20190608&state=${sc}`
+    let year = document.querySelector("#year_name").selectedOptions[0].value;
+    changeTitle(year);
+    let url = `https://aqs.epa.gov/data/api/monitors/byState?email=baacer01@luther.edu&key=ecruhare55&param=88101&bdate=${year}0608&edate=${year}0608&state=${sc}`
     let sitedata = await fetch(url)
             .then(response => response.json())
             .catch(error => console.log(error));
@@ -77,16 +97,17 @@ async function getSites(){
 async function getData() {
     let state = document.querySelector("#state_name").selectedOptions[0].value;
     let cd = document.querySelector("#site_name").selectedOptions[0].value;
+    let year = document.querySelector("#year_name").selectedOptions[0].value;
     // console.log(cd);
     let cdn = cd.split(",");
     let county = cdn[1];
     let site = cdn[2]
-    console.log(state, county, site)
-    let GPS_list = await getGPS(state, county, site);
+    console.log(state, county, site, year)
+    let GPS_list = await getGPS(state, county, site, year);
     // console.log("GPS", GPS_list);
-    let air_stats = await get_air(state, county, site);
+    let air_stats = await get_air(state, county, site, year);
     // console.log("AQ", air_stats);
-    let census_stats = await get_census(state, county);
+    let census_stats = await get_census(state, county, year);
     // console.log("Census", census_stats);
     draw(census_stats)
     lineChart(air_stats)
@@ -94,8 +115,8 @@ async function getData() {
 }
 
 // Get latitude and longitude
-async function getGPS(state, county, site){
-    let url = `https://aqs.epa.gov/data/api/annualData/bySite?email=baacer01@luther.edu&key=ecruhare55&param=88101&bdate=20190618&edate=20190618&state=${state}&county=${county}&site=${site}`;
+async function getGPS(state, county, site, year){
+    let url = `https://aqs.epa.gov/data/api/annualData/bySite?email=baacer01@luther.edu&key=ecruhare55&param=88101&bdate=${year}0618&edate=${year}0618&state=${state}&county=${county}&site=${site}`;
     // console.log("GPS url", url);
     let data = await fetch(url)
             .then(response => response.json())
@@ -106,9 +127,9 @@ async function getGPS(state, county, site){
 
 // Get air quality data
 // Return average for that site for the year and fraction of days that exceed the limit
-async function get_air(state, county, site) {
+async function get_air(state, county, site, year) {
     
-    let url = `https://aqs.epa.gov/data/api/dailyData/bySite?email=baacer01@luther.edu&key=ecruhare55&param=88101&bdate=20190101&edate=20191231&state=${state}&county=${county}&site=${site}`;
+    let url = `https://aqs.epa.gov/data/api/dailyData/bySite?email=baacer01@luther.edu&key=ecruhare55&param=88101&bdate=${year}0101&edate=${year}1231&state=${state}&county=${county}&site=${site}`;
     // console.log("AQ url", url);
     let airdata = await fetch(`${url}`).then(response => response.json());
     // let aqmean = aqtotal / aqctr;
@@ -119,8 +140,8 @@ async function get_air(state, county, site) {
 }
 
 // Get race
-async function get_census(state, county) {
-    let url = `https://api.census.gov/data/2019/acs/acs5?get=NAME,B03001_002E,B02001_003E,B02001_004E,B02001_005E,B03001_003E&for=county:${county}&in=state:${state}&key=654a975e290481a70449f6c9bf6a9c21fe3ea0ff`;
+async function get_census(state, county, year) {
+    let url = `https://api.census.gov/data/${year}/acs/acs5?get=NAME,B03001_002E,B02001_003E,B02001_004E,B02001_005E,B03001_003E&for=county:${county}&in=state:${state}&key=654a975e290481a70449f6c9bf6a9c21fe3ea0ff`;
     // console.log("Census url", url);
     let census_data = await fetch(`${url}`).then(response => response.json());
     return [census_data[1][1], census_data[1][2], census_data[1][3], census_data[1][4], census_data[1][5]];
